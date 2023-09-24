4af691c302284e4becef9c2c587dafffeefc1d45
YahyaNashar22 <cenations.people@live.com>
2023-09-24 13:28:11 +0300

    step 6

--- movies-route.js
+++ movies-route.js
@@ -3,8 +3,59 @@ const router = express.Router();
 const movies = require("./movies-object");
 router.get("/read", (req, res, next) => {
   res.status(200).json({
+    status: "200",
     message: "Welcome to the movie section",
     data: movies,
+  });
+});
+router.get("/read/by-date", (req, res, next) => {
+  let sortedByDate = movies.sort(function (a, b) {
+    if (a.year < b.year) {
+      return -1;
+    }
+    if (a.year > b.year) {
+      return 1;
+    }
+    return 0;
+  });
+  res.status(200).json({
+    status: "200",
+    message: "Movies Sorted By Date",
+    data: sortedByDate,
+  });
+});
+router.get("/read/by-rating", (req, res, next) => {
+  let sortedByRating = movies.sort(function (a, b) {
+    if (a.rating < b.rating) {
+      return 1;
+    }
+    if (a.rating > b.rating) {
+      return -1;
+    }
+    return 0;
+  });
+  res.status(200).json({
+    status: "200",
+    message: "Movies Sorted By Rating",
+    data: sortedByRating,
+  });
+});
+router.get("/read/by-title", (req, res, next) => {
+  let sortedByTitle = movies.sort(function (a, b) {
+    let x = a.title.toUpperCase();
+    let y = b.title.toUpperCase();
+    if (x < y) {
+      return -1;
+    }
+    if (x > y) {
+      return 1;
+    }
+    return 0;
+  });
+  res.status(200).json({
+    status: "200",
+    message: "Movies Sorted By Title",
+    data: sortedByTitle,
   });
 });
 router.post("/create", (req, res, next) => {
